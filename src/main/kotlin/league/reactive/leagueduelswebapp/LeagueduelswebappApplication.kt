899293a7c9caed7508effa0c5c0d9e495c5809ba
ÍcaroJurado <icaro.jurado@mercadolivre.com>
2021-07-12 12:06:28 -0300

    first commit

---
+++ src/main/kotlin/league/reactive/leagueduelswebapp/LeagueduelswebappApplication.kt
@@ -0,0 +1,11 @@
+package league.reactive.leagueduelswebapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeagueduelswebappApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeagueduelswebappApplication>(*args)
+}
---
+++ src/test/kotlin/league/reactive/leagueduelswebapp/LeagueduelswebappApplicationTests.kt
@@ -0,0 +1,13 @@
+package league.reactive.leagueduelswebapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeagueduelswebappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
